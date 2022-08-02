edc862efe0fdc7c24a1010735c83aaadc1f1c158
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-02 19:39:55 +0200

    added infinit scroll functionality + search functionality

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Photo = () => {
-  return <h2>photo component</h2>;
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={regular} alt={alt_description} />
+      <div className='photo-info'>
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} Likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt="user"/>
+        </a>
+      </div>
+    </article>
+  );
 };
 
 export default Photo;
